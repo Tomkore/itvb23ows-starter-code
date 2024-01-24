41b918b140a3246976b5e2a78104f29c16d7b082
Tomkore <tom.reezigt@gmail.com>
2024-01-24 15:13:12 +0100

    fixing app db 15

--- restart.php
+++ restart.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-echo "Iets";
+print "Iets";
 
 $_SESSION['board'] = [];
 $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
