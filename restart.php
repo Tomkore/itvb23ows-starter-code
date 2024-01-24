18d1d4e759163f83c32c490f5e2e6fcc2f133f8f
Tomkore <tom.reezigt@gmail.com>
2024-01-24 15:57:07 +0100

    fixing app db 19

--- restart.php
+++ restart.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-print "Iets";
+
 
 $_SESSION['board'] = [];
 $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
@@ -8,6 +8,9 @@ $_SESSION['player'] = 0;
 
 
 $db = include_once 'database.php';
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
 $db->prepare('INSERT INTO games VALUES ()')->execute();
 $_SESSION['game_id'] = $db->insert_id;
 
