9dfda3c0c016be079ae3358455fbe86d6ad1dd40
Tomkore <tom.reezigt@gmail.com>
2024-01-26 17:49:04 +0100

    fix db input in restart.php

--- database.php
+++ database.php
@@ -11,7 +11,7 @@ function set_state($state) {
     $_SESSION['player'] = $c;
 }
 
-$mysqli = new mysqli('db:3306', 'root', 'root', 'hive');
+$mysqli = new mysqli('db', 'root', 'root', 'hive', 3306);
 $mysqli->set_charset("utf8mb4");
 return $mysqli;
 
