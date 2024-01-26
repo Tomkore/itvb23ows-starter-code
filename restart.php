a1aedd93b00b9b7d1e155d6b5042e1ebcd78e824
Tomkore <tom.reezigt@gmail.com>
2024-01-26 15:57:32 +0100

    fixing app db 19

--- restart.php
+++ restart.php
@@ -8,12 +8,12 @@ $_SESSION['player'] = 0;
 
 
 $db = include_once 'database.php';
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
-else {
-    echo "connected";
-}
+//if ($db->connect_error) {
+//    die("Connection failed: " . $db->connect_error);
+//}
+//else {
+//    echo "connected";
+//}
 $db->prepare('INSERT INTO games VALUES ()')->execute();
 $_SESSION['game_id'] = $db->insert_id;
 
